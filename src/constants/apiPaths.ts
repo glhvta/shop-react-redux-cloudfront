04cce6a4065106c40285498f249ee345f8aae89d
Tatsiana <tatsiana_helakhava@epam.com>
2022-10-06 01:15:23 +0300

    Update import service path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://bta8xv58vl.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://ddrqkjfpj2.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://lrp3td9umf.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://bta8xv58vl.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
